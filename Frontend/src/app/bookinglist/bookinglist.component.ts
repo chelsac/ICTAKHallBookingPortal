93430bca2d8066958ade037a79bf768c48fccb2f
chelsac <chelsacleetuscc@gmail.com>
2022-10-17 23:22:01 +0530

    add booking

--- Frontend/src/app/bookinglist/bookinglist.component.ts
+++ Frontend/src/app/bookinglist/bookinglist.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BookingService } from '../services/booking/booking.service';
 @Component({
+
   selector: 'app-bookinglist',
   templateUrl: './bookinglist.component.html',
   styleUrls: ['./bookinglist.component.css'],
 })
 export class BookinglistComponent implements OnInit {
+  booking
+  ={
+    userid:"",
+    name:"",
+    hallname:"",
+    date:"",
+    starttime:"",
+    endtime:"",
+    status:"pending"
+  };
 
-  constructor() {}
+  addbookingfn(){
+    var date=new  Date(this.booking.date);
+    this.booking.date=date.toLocaleDateString();
+    console.log(this.booking.date);
+    return this.bookingservice.addbooking(this.booking)
+    .subscribe((res)=>{
+      alert("Booking succesfully added");
+      // location.pathname = ('/conferenceRooms');
+    })
+  }
+
+  constructor(private bookingservice:BookingService,private router: Router ) {}
 
   ngOnInit(): void {}
 }
