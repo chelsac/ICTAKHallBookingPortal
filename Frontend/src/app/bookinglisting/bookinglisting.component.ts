3b02e7e362956628db22d1f1b8bba1ddaa0a1ee3
chelsac <chelsacleetuscc@gmail.com>
2022-10-21 19:56:00 +0530

    booking list user

--- Frontend/src/app/bookinglisting/bookinglisting.component.ts
+++ Frontend/src/app/bookinglisting/bookinglisting.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BookingService } from '../services/booking/booking.service';
+import { Router } from '@angular/router';
+import { UserService } from '../services/users/user.service';
 
 @Component({
   selector: 'app-bookinglisting',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookinglistingComponent implements OnInit {
 
-  constructor() { }
+  date = new Date();
+  allbooking=[{
+    userid:"",
+    name:"",
+    hallname:"",
+    date:"",
+    starttime:"",
+    endtime:"",
+    status:""
+  }];
+  booking
+  =[{
+    userid:"",
+    name:"",
+    hallname:"",
+    date:"",
+    starttime:"",
+    endtime:"",
+    status:""
+  }];
+
+  constructor(private bookingservice:BookingService,private userservice:UserService,private router: Router) { }
 
   ngOnInit(): void {
+    
+    var emailid= localStorage.getItem("emailid");
+    console.log(emailid);
+    this.userservice.getbooking(emailid).subscribe((data:any)=>{
+      console.log(data.body.userid);
+      this.getbookingdetails(data.body.userid);
+  })
+  }
+
+  getbookingdetails(userid:string){
+    this.bookingservice.getbookingweek(userid).subscribe((data) => {
+      this.allbooking = JSON.parse(JSON.stringify(data));
+      this.getbookingvalid(this.allbooking);
+    })
+  }
+
+  getbookingvalid(data:any){
+    this.booking.pop();
+    const date = new Date();
+    for(var i in data){
+      const bookingdate=new Date(data[i].date);
+      if(date<=bookingdate){
+        this.booking.push(data[i]);
+      }
+    }
   }
 
 }
