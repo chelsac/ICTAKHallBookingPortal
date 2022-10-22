954313435eaa7c51d305d8e3ddbc66d27fb57dd7
chelsac <chelsacleetuscc@gmail.com>
2022-10-22 16:40:11 +0530

    home

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BookingService } from '../services/booking/booking.service';
+import { Router } from '@angular/router';
+import { UserService } from '../services/users/user.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  allbooking=[{
+    userid:"",
+    name:"",
+    hallname:"",
+    date:"",
+    starttime:"",
+    endtime:"",
+    status:""
+  }];
+  booking
+  =[{
+    userid:"",
+    name:"",
+    hallname:"",
+    date:"",
+    starttime:"",
+    endtime:"",
+    status:""
+  }];
 
-  constructor() { }
+  constructor(private bookingservice:BookingService,private userservice:UserService,private router: Router) { }
 
   ngOnInit(): void {
+    this.bookingservice.getallbooking().subscribe((data) => {
+      this.allbooking = JSON.parse(JSON.stringify(data));
+      console.log(this.allbooking);
+      this.bookingpending(this.allbooking);
+    })
+  }
+
+  bookingpending(data:any){
+    this.booking.pop();
+    for(var i in data){
+      if(data[i].status=="pending"){
+        this.booking.push(data[i]);
+      }
+    }
+    console.log(this.booking);
   }
 
   more(){
